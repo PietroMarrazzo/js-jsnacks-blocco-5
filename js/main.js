567355d202d4f2824c57a1bf4d48486d306dffb8
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-02 14:34:43 +0100

    snack1 - fixing

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
 $(document).ready(function(){
 
-    // snack 1
+    // SNACK 1
     var pesci = [
         {
             nome: 'tonno',
-            peso: 10,
+            peso: 10
         },
         {
             nome: 'salmone',
-            peso: 5,
+            peso: 5
         },
         {
             nome: 'branzino',
-            peso: 8,
+            peso: 8
         }, 
         {
             nome: 'orata',
-            peso: 3,
-        },   
-];
+            peso: 3
+        }
+    ];
 
-var pesceLeggero = pesci[0]
+    var pesceLeggero = pesci[0];
 
-for (i = 1; i < pesci.length; i++) {
+    for (i = 1; i < pesci.length; i++) {
 
-    if ( pesceLeggero.peso > pesci[i].peso) {
-        pesceLeggero = pesci[i];
+        if ( pesceLeggero.peso > pesci[i].peso) {
+            pesceLeggero = pesci[i];
+        }
     }
-}
-console.log(pesceLeggero);
+    // console.log(pesceLeggero);
+
+    // handlebars script
+    var source = $('#fish-template').html;
+
+    var template = Handlebars.compile(source);
+
+    // stampa a schermo
+    var data = [
+        {
+        nome: pesceLeggero.nome,
+        peso: pesceLeggero.peso,
+    }
+    ]
+
+    // compilazione
+    var html = template(data);
+
+    // inserimento in html della compilazione
+    $('.container').append(html);
+
+
+
+
+    // SNACK 2
+
+
 
 
 
